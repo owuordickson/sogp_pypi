059fc5c232004d0efdfe49780a34dfa20fc9ca68
owuordickson <owuordickson@gmail.com>
2021-09-14 19:33:05 +0300

    added

--- src/so4gp_pkg/so4gp.py
+++ src/so4gp_pkg/so4gp.py
@@ -21,6 +21,8 @@ import gc
 import numpy as np
 # import json
 import pandas as pd
+import random
+from ypstruct import structure
 
 
 # -------- CONFIGURATION (START)-------------
@@ -470,7 +472,6 @@ def run_ant_colony(f_path, min_supp=MIN_SUPPORT, evaporation_factor=EVAPORATION_
     repeated = 0
     it_count = 0
     counter = 0
-    max_it = max_iteration
 
     if d_set.no_bins:
         return []
@@ -484,7 +485,7 @@ def run_ant_colony(f_path, min_supp=MIN_SUPPORT, evaporation_factor=EVAPORATION_
 
     # 4. Iterations for ACO
     # while repeated < 1:
-    while counter < max_it:
+    while counter < max_iteration:
         rand_gp, pheromones = generate_aco_gp(attr_keys, d, pheromones, evaporation_factor)
         if len(rand_gp.gradual_items) > 1:
             # print(rand_gp.get_pattern())
@@ -513,7 +514,7 @@ def run_ant_colony(f_path, min_supp=MIN_SUPPORT, evaporation_factor=EVAPORATION_
             else:
                 repeated += 1
         it_count += 1
-        if max_it == 1:
+        if max_iteration == 1:
             counter = repeated
         else:
             counter = it_count
@@ -622,6 +623,239 @@ def is_duplicate(pattern, lst_winners, lst_losers):
 
 
 # -------- GA-GRAD (START)----------
+
+
+"""
+@author: "Dickson Owuor"
+@credits: "Thomas Runkler, and Anne Laurent,"
+@license: "MIT"
+@version: "2.0"
+@email: "owuordickson@gmail.com"
+@created: "29 April 2021"
+@modified: "07 September 2021"
+Breath-First Search for gradual patterns using Genetic Algorithm (GA-GRAD).
+GA is used to learn gradual pattern candidates.
+CHANGES:
+1. uses normal functions
+2. updated cost function to use Binary Array of GPs
+3. uses rank order search space
+"""
+
+
+# import random
+
+
+def run_genetic_algorithm(data_src, min_supp=MIN_SUPPORT, max_iteration=MAX_ITERATIONS, n_pop=N_POPULATION, pc=PC,
+                          gamma=GAMMA, mu=MU, sigma=SIGMA):
+    # Prepare data set
+    d_set = Dataset(data_src, min_supp)
+    d_set.init_gp_attributes()
+    attr_keys = [GI(x[0], x[1].decode()).as_string() for x in d_set.valid_bins[:, 0]]
+
+    if d_set.no_bins:
+        return []
+
+    # Problem Information
+    # cost_func
+
+    # Parameters
+    # pc: Proportion of children (if its 1, then nc == npop
+    it_count = 0
+    eval_count = 0
+    counter = 0
+    var_min = 0
+    var_max = int(''.join(['1'] * len(attr_keys)), 2)
+
+    nc = int(np.round(pc * n_pop / 2) * 2)  # Number of children. np.round is used to get even number of children
+
+    # Empty Individual Template
+    empty_individual = structure()
+    empty_individual.position = None
+    empty_individual.cost = None
+
+    # Initialize Population
+    pop = empty_individual.repeat(n_pop)
+    for i in range(n_pop):
+        pop[i].position = random.randrange(var_min, var_max)
+        pop[i].cost = 1  # cost_func(pop[i].position, attr_keys, d_set)
+        # if pop[i].cost < best_sol.cost:
+        #    best_sol = pop[i].deepcopy()
+
+    # Best Solution Ever Found
+    best_sol = empty_individual.deepcopy()
+    best_sol.position = pop[0].position
+    best_sol.cost = cost_func(best_sol.position, attr_keys, d_set)
+
+    # Best Cost of Iteration
+    best_costs = np.empty(max_iteration)
+    best_patterns = list()
+    str_iter = ''
+    str_eval = ''
+
+    repeated = 0
+    while counter < max_iteration:
+        # while eval_count < max_evaluations:
+        # while repeated < 1:
+
+        c_pop = []  # Children population
+        for _ in range(nc // 2):
+            # Select Parents
+            q = np.random.permutation(n_pop)
+            p1 = pop[q[0]]
+            p2 = pop[q[1]]
+
+            # a. Perform Crossover
+            c1, c2 = crossover(p1, p2, gamma)
+
+            # Apply Bound
+            apply_bound(c1, var_min, var_max)
+            apply_bound(c2, var_min, var_max)
+
+            # Evaluate First Offspring
+            c1.cost = cost_func(c1.position, attr_keys, d_set)
+            if c1.cost < best_sol.cost:
+                best_sol = c1.deepcopy()
+            eval_count += 1
+            str_eval += "{}: {} \n".format(eval_count, best_sol.cost)
+
+            # Evaluate Second Offspring
+            c2.cost = cost_func(c2.position, attr_keys, d_set)
+            if c2.cost < best_sol.cost:
+                best_sol = c2.deepcopy()
+            eval_count += 1
+            str_eval += "{}: {} \n".format(eval_count, best_sol.cost)
+
+            # b. Perform Mutation
+            c1 = mutate(c1, mu, sigma)
+            c2 = mutate(c2, mu, sigma)
+
+            # Apply Bound
+            apply_bound(c1, var_min, var_max)
+            apply_bound(c2, var_min, var_max)
+
+            # Evaluate First Offspring
+            c1.cost = cost_func(c1.position, attr_keys, d_set)
+            if c1.cost < best_sol.cost:
+                best_sol = c1.deepcopy()
+            eval_count += 1
+            str_eval += "{}: {} \n".format(eval_count, best_sol.cost)
+
+            # Evaluate Second Offspring
+            c2.cost = cost_func(c2.position, attr_keys, d_set)
+            if c2.cost < best_sol.cost:
+                best_sol = c2.deepcopy()
+            eval_count += 1
+            str_eval += "{}: {} \n".format(eval_count, best_sol.cost)
+
+            # c. Add Offsprings to c_pop
+            c_pop.append(c1)
+            c_pop.append(c2)
+
+        # Merge, Sort and Select
+        pop += c_pop
+        pop = sorted(pop, key=lambda x: x.cost)
+        pop = pop[0:n_pop]
+
+        best_gp = validate_gp(d_set, decode_gp(attr_keys, best_sol.position))
+        is_present = is_duplicate(best_gp, best_patterns)
+        is_sub = check_anti_monotony(best_patterns, best_gp, subset=True)
+        if is_present or is_sub:
+            repeated += 1
+        else:
+            if best_gp.support >= min_supp:
+                best_patterns.append(best_gp.print(d_set.titles))
+            # else:
+            #    best_sol.cost = 1
+
+        try:
+            # Show Iteration Information
+            # Store Best Cost
+            best_costs[it_count] = best_sol.cost
+            str_iter += "{}: {} \n".format(it_count, best_sol.cost)
+        except IndexError:
+            pass
+        it_count += 1
+
+        if max_iteration == 1:
+            counter = repeated
+        else:
+            counter = it_count
+    # Output
+    out = {"Best Patterns": best_patterns, "Iterations": it_count}
+    return out
+
+
+def cost_func(position, attr_keys, d_set):
+    pattern = decode_gp(attr_keys, position)
+    temp_bin = np.array([])
+    for gi in pattern.gradual_items:
+        arg = np.argwhere(np.isin(d_set.valid_bins[:, 0], gi.gradual_item))
+        if len(arg) > 0:
+            i = arg[0][0]
+            valid_bin = d_set.valid_bins[i]
+            if temp_bin.size <= 0:
+                temp_bin = valid_bin[1].copy()
+            else:
+                temp_bin = np.multiply(temp_bin, valid_bin[1])
+    bin_sum = np.sum(temp_bin)
+    if bin_sum > 0:
+        cost = (1 / bin_sum)
+    else:
+        cost = 1
+    return cost
+
+
+def crossover(p1, p2, gamma=0.1):
+    c1 = p1.deepcopy()
+    c2 = p2.deepcopy()
+    alpha = np.random.uniform(0, gamma, 1)
+    c1.position = alpha*p1.position + (1-alpha)*p2.position
+    c2.position = alpha*p2.position + (1-alpha)*p1.position
+    return c1, c2
+
+
+def mutate(x, mu, sigma):
+    y = x.deepcopy()
+    str_x = str(int(y.position))
+    # flag = np.random.rand(*x.position.shape) <= mu
+    # ind = np.argwhere(flag)
+    # y.position[ind] += sigma*np.random.rand(*ind.shape)
+    flag = np.random.rand(*(len(str_x),)) <= mu
+    ind = np.argwhere(flag)
+    str_y = "0"
+    for i in ind:
+        val = float(str_x[i[0]])
+        val += sigma*np.random.uniform(0, 1, 1)
+        if i[0] == 0:
+            str_y = "".join(("", "{}".format(int(val)), str_x[1:]))
+        else:
+            str_y = "".join((str_x[:i[0] - 1], "{}".format(int(val)), str_x[i[0]:]))
+        str_x = str_y
+    y.position = int(str_y)
+    return y
+
+
+def apply_bound(x, var_min, var_max):
+    x.position = np.maximum(x.position, var_min)
+    x.position = np.minimum(x.position, var_max)
+
+
+def decode_gp(attr_keys, position):
+    temp_gp = GP()
+    if position is None:
+        return temp_gp
+
+    bin_str = bin(int(position))[2:]
+    bin_arr = np.array(list(bin_str), dtype=int)
+
+    for i in range(bin_arr.size):
+        bin_val = bin_arr[i]
+        if bin_val == 1:
+            gi = GI.parse_gi(attr_keys[i])
+            if not temp_gp.contains_attr(gi):
+                temp_gp.add_gradual_item(gi)
+    return temp_gp
+
 
 # -------- GA-GRAD (END)-------------
 
